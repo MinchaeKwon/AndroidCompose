6033d472a42a123231f4e411d669624c8189830b
권민채 <minchae075@naver.com>
2024-01-30 19:39:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/androidcompose/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.androidcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androidcompose.ui.theme.AndroidComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier) {
+    Text(
+        text = message,
+        fontSize = 100.dp,
+        lineHeight = 116.dp,
+    )
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    AndroidComposeTheme {
+        GreetingText(message = "Happy Birthday Lion!")
+    }
+}
